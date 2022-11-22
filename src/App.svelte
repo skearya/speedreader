0bfd7a77db3f09fbe6d541dafe3e86e95d89a37b
skearya <77034153+skearya@users.noreply.github.com>
2022-11-22 18:47:33 -0500

    more css, replaced slider, added space to pause

--- src/App.svelte
+++ src/App.svelte
@@ -9,7 +9,13 @@
   let text = "speedreader";
   let input = "";
   let running = false;
-  let wpm = 500;
+  let wpm = 400;
+
+  function handleKeydown(event) {
+    if (event.code == "Space") {
+      paused = !paused;
+    }
+  }
 
   async function render() {
     if (input == "") {
@@ -17,6 +23,7 @@
       return;
     }
 
+    paused = false;
     running = true;
     const myArray = input.split(" ");
 
@@ -32,6 +39,8 @@
   }
 </script>
 
+<svelte:window on:keydown={handleKeydown} />
+
 <main>
   <h1>{text}</h1>
 
@@ -45,13 +54,51 @@
 
   {#if !running}
     <div>
-      <textarea placeholder="insert text here" bind:value={input} />
+      <textarea id="text" placeholder="insert text here" bind:value={input} />
     </div>
   {/if}
 
-  <p>wpm: {wpm}</p>
-  <input type="range" bind:value={wpm} min="1" max="1000" />
+  <div>
+    <label for="wpm">words per minute </label>
+    <input
+      id="wpm"
+      type="number"
+      bind:value={wpm}
+      min="0"
+      max="1000"
+      step="100"
+    />
+  </div>
 </main>
 
 <style>
+  button {
+    margin-top: 0.3rem;
+    margin-bottom: 1.5rem;
+  }
+
+  ::placeholder {
+    text-align: center;
+    height: 30px;
+    line-height: 30px;
+  }
+  #text,
+  #wpm {
+    border-radius: 8px;
+    border: 1px solid transparent;
+    margin-bottom: 1.3rem;
+    padding: 0.6em;
+    background-color: #1a1a1a;
+    cursor: pointer;
+    transition: border-color 0.25s;
+  }
+  #text:hover,
+  #wpm:hover {
+    border-color: #646cff;
+  }
+
+  #wpm {
+    width: 3rem;
+    margin-left: 0.4rem;
+  }
 </style>
